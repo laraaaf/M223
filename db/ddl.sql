7410018d584f2c67731d62a627e84a2be7bd162b
Lara Felix <lafel2002@gmail.com>
2021-11-30 16:29:24 +0100

    add arbeitsjournal tag 2, update with umls

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,15 +1,33 @@
 /* 
 Author: Lara Felix
-Titel: M223
+Titel: M223 - FoldingPaperStory
 */
 
 -- DATABASE
-DROP DATABASE IF EXISTS DataWarehouse;
-CREATE DATABASE DataWarehouse;
-USE DataWarehouse;
+DROP DATABASE IF EXISTS FoldingPaperStory;
+CREATE DATABASE FoldingPaperStory;
+USE FoldingPaperStory;
 
 -- CREATE TABLES
-CREATE TABLE tbl_filiale (
-  ID_Filiale INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  FilialenName VARCHAR(50)
+CREATE TABLE tbl_User (
+  ID_User INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Mail VARCHAR(50),
+  Password VARCHAR(50),
+  Rolle Int
+);
+
+CREATE TABLE tbl_Story (
+  ID_StoryLine INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  sentence1 VARCHAR(100),
+  sentence2 VARCHAR(100)
+);
+
+
+CREATE TABLE tbl_UserStory (
+  ID_StoryLine INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  timestamp DATE NOT NULL,
+  FK_User INT UNSIGNED,
+  FK_StoryLine INT UNSIGNED,
+  FOREIGN KEY (FK_User) REFERENCES tbl_User(ID_User),
+  FOREIGN KEY (FK_StoryLine) REFERENCES tbl_Story(ID_StoryLine)
 );
